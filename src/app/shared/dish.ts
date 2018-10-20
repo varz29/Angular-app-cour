87df69abd3aa8376d35f809bcbc591cb66623e45
varz29 <vk82786@gmail.com>
2018-10-20 19:09:35 +0530

    menu component

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,12 @@
+import { StringifyOptions } from "querystring";
+
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
