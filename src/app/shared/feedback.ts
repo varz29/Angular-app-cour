5ee1fba874a5a9f0d1cd6c51c3c421bdfe49c494
varz29 <vk82786@gmail.com>
2018-11-15 18:09:54 +0530

    Added Reactive Forms for feed back

---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,11 @@
+export class Feedback {
+    firstnmae: string;
+    lastname: string;
+    telnum: string;
+    email: string;
+    agree: boolean;
+    contacttype: string;
+    message: string;
+}
+
+export const ContactType = ['none', 'Tel', 'Email'];
